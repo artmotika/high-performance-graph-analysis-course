adff3bc2d19e4a2cd69ab64a08d7ac750b51d668
Motika Artem <artmotika@gmail.com>
2023-03-15 05:10:46 +0300

    test

---
+++ project/graph_utils.py
@@ -0,0 +1,33 @@
+import networkx as nx
+import pygraphblas as gb
+
+__all__ = ["read_got", "digraph_to_matrix_gb"]
+
+
+def read_dot(file_path: str) -> nx.DiGraph:
+    """
+    Read dot file and get MultiGraph from networkx
+
+    :param file_name: name of the dot file
+    :return: DiGraph (directed graph) from networkx
+    """
+
+    return nx.DiGraph(nx.nx_pydot.read_dot(file_path))
+
+
+def digraph_to_matrix_gb(graph: nx.DiGraph) -> gb.Matrix:
+    """
+    Convert DiGraph from networkx to boolean adjacency matrix from pygraphblas
+
+    :param graph: DiGraph (directed graph) from networkx
+    :return: boolean adjacency matrix from pygraphblas
+    """
+
+    size = graph.number_of_nodes()
+    Matrix = gb.sparse(gb.types.BOOL, size, size)
+    for i, s in enumerate(graph.nodes):
+        node_to_idx[s] = i
+    for source, target in graph.edges():
+        Matrix[node_to_idx[source], node_to_idx[target]] = True
+
+    return Matrix
---
+++ tests/test_bfs.py
@@ -0,0 +1,8 @@
+import pytest
+import pygraphblas as gb
+
+
+def test_bfs_loop():
+    assert bfs(digraph_to_matrix_gb(read_dot("data/graph_loop.dot")), 0) == np.array(
+        [0, 1]
+    )
