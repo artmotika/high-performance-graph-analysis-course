044c4eec178f35c23b3dd38bc55fb0d3a1c6b93a
Motika Artem <artmotika@gmail.com>
2023-03-15 21:23:40 +0300

    .

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -5,11 +5,13 @@ import numpy as np
 
 path = pathlib.Path(__file__).parent / "data" / "test_bfs.json"
 test_data = load_test_data(path)
+
+
 def test_bfs1():
     matrix, source, expected = test_data[0]
     assert np.array_equal(bfs(matrix, source), np.asarray(expected))
 
+
 def test_bfs2():
     matrix, source, expected = test_data[1]
     assert np.array_equal(bfs(matrix, source), np.asarray(expected))
-
