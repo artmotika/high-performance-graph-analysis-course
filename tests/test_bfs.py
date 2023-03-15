ff45322996964237d6be5dc2ed0fb3df2f9c4a9c
Motika Artem <artmotika@gmail.com>
2023-03-15 05:25:02 +0300

    Update test_bfs.py

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -6,5 +6,5 @@ import networkx as nx
 
 def test_bfs_loop():
     assert bfs(
-        digraph_to_matrix_gb(read_dot_file("data/graph_loop.dot")), 0
+        digraph_to_matrix_gb(nx.DiGraph(nx.nx_pydot.read_dot("data/graph_loop.dot"))), 0
     ) == np.array([0, 1])
